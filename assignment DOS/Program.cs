187d876a700a3d899afd119cc74f6d1ffc6ec8bb
kirubel <kirubeltesfaye19@gmail.com>
2018-04-04 10:22:39 -0500

    new assignment

---
+++ assignment DOS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+
+namespace dosattake
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			for (; ; )
+			{
+				 
+				Console.Write("Write url : ");
+				string x = Console.ReadLine();
+				while (true)
+				{
+					
+						HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(x);
+
+						Console.WriteLine(x);
+				
+				}
+				
+			}
+			}
+	}
+}
